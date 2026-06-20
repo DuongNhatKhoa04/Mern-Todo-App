3898e9abc8969ef5ab0fa520859b86717a989f3b
KhoaDN <nhatkhoa.working@gmail.com>
2026-06-21 02:54:40 +0700

    [jenkins]production

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
         <div>
             <nav className='header bg-slate-200 flex justify-between items-center'>
                 <div className="logo w-1/4 text-center">
-                    <NavLink to="/">Todo App Test GithubActions</NavLink>
+                    <NavLink to="/">Todo App By Jenkins</NavLink>
                 </div>
                 <div className='flex justify-between'>
                     {
